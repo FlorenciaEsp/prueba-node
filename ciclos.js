d0a879e52aed1dce5ac53bfae3a2ea43be8d88fd
FlorenciaEsp <florencia.esposito@gmail.com>
2024-01-31 20:12:53 +0100

    Primeros dos archivos

---
+++ ciclos.js
@@ -0,0 +1,14 @@
+// for(let i = 0; i<10; i++){
+//     console.log("El valor de i es igual a: " +i);
+// }
+
+function tablaDeMultiplicar(numero) {
+    let i = 1;
+    while (1 <= 10) {
+        console.log (numero + " " + "*" + " " + i + " " +  "=" + " " + i * numero);
+        i++;
+    };
+    return i;  
+};
+
+// Prueb git
---
+++ funciones.js
@@ -0,0 +1,40 @@
+// Función expresada
+
+let sumar = function(numeroA, numeroB) {
+    return numeroA + numeroB;
+
+}
+
+console.log (sumar(7 , 9));
+
+
+
+// Función declarada
+
+function restar (numeroC, numeroD) {
+    return numeroC - numeroD;
+}
+
+console.log (restar(10,3));
+
+// Ejemplo de Scope
+
+let mensaje = "Hola";
+
+function saludar () {
+    let mensaje = "Chau";
+    return mensaje;
+}
+
+console.log(mensaje);
+console.log(saludar());
+
+//Practica
+
+function multiplicar(numeroA,numeroB) {
+    return numeroA * numeroB;
+}
+
+multiplicar(5*5);
+console.log(multiplicar(5,5));
+
